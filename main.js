9a6b2915aad59ce5ace9afa9bca0a89346554e68
Oliver Friedrich <friedrich@contentbird.io>
2024-08-08 11:32:24 +0200

    GFR-001 just fix some jslint hints

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const request = require('request')
-const express = require('express')
-const app = express()
-const args = process.argv.slice(2)
-const replaced_host = args[0] || 'fonts.gstatic.com'
-const port = args[1] || 3000
+/* global process */
+
+const request = require('request');
+const express = require('express');
+const app = express();
+const args = process.argv.slice(2);
+const replaced_host = args[0] || 'fonts.gstatic.com';
+const port = args[1] || 3000;
 
 app.get('/css', function (req, res) {
-    const url = "https://fonts.googleapis.com" + req.originalUrl
+    const url = "https://fonts.googleapis.com" + req.originalUrl;
 
     request.get({
         url: url,
         headers: {
-            'User-Agent': req.headers['user-agent'],
+            'User-Agent': req.headers['user-agent']
         }
     },
     function (error, response, body) {
@@ -21,12 +23,12 @@ app.get('/css', function (req, res) {
 
         res.set({
             'content-type': 'text/css; charset=utf-8'
-        })
-        res.send(response.body.replace(new RegExp('fonts.gstatic.com', 'g'), replaced_host))
+        });
+        res.send(response.body.replace(new RegExp('fonts.gstatic.com', 'g'), replaced_host));
     });
-})
+});
 
 app.disable('x-powered-by');
 app.listen(port, function () {
-  console.log('Google Fonts Resolver is listening on port ' + port + ' !')
-})
+  console.log('Google Fonts Resolver is listening on port ' + port + ' !');
+});
